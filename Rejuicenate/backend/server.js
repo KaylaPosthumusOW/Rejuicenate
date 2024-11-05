c8314e037236413138b405fbbd49bd2219cc8b45
KaylaPosthumusOW <231096@virtualwindow.co.za>
2024-11-05 07:34:08 +0200

    update package.jsons

--- Rejuicenate/backend/server.js
+++ Rejuicenate/backend/server.js
@@ -24,10 +24,6 @@ app.get('/', (req, res) => {
   res.send('Backend is running!');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
-
 // Serve static files from the 'uploads' folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
