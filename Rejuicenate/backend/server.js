29d38039b3a38a71fc0893d2b89bc4db43131877
KaylaPosthumusOW <231096@virtualwindow.co.za>
2024-11-05 08:20:54 +0200

    update server.js

--- Rejuicenate/backend/server.js
+++ Rejuicenate/backend/server.js
@@ -15,10 +15,12 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
-
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);  // Exit if unable to connect
+  });
 // Basic route
 app.get('/', (req, res) => {
   res.send('Backend is running!');
